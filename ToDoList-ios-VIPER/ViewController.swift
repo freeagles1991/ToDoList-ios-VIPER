ec68a8509c7a119d1a6ed013b892af4672645d83
Freeagles1991 <dmitry.nerovny@gmail.com>
2024-11-25 18:12:11 +0300

    Initial Commit

---
+++ ToDoList-ios-VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList-ios-VIPER
+//
+//  Created by Дима on 25.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
